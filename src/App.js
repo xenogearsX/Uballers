9f24c242f168b8c0c4d68d282d1cbcb81c982b17
xenogearsX <xenogears@hotmail.fr>
2021-06-27 20:31:58 +0200

    Link all the screens with react-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 
+import { Route, Switch } from 'react-router'
+
+import Favorites from './screens/Favorites'
+import GroundDetails from './screens/GroundDetails'
+import Header from './components/Header'
+import Home from './screens/Home'
 import UserContext from './context/UserContext'
+
+import groundsData from './data/json/groundsData.json'
 
 import './App.css'
 
@@ -9,9 +17,14 @@ const App = () => {
 
   return (
     <div className='App'>
-      <UserContext.Provider
-        value={[favorite, setFavorite]}
-      ></UserContext.Provider>
+      <UserContext.Provider value={[groundsData, favorite, setFavorite]}>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/ground/:id' component={GroundDetails} />
+          <Route path='/favorites' component={Favorites} />
+        </Switch>
+      </UserContext.Provider>
     </div>
   )
 }
