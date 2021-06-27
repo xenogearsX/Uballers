d1c113d11f5d2434138be855193cdda7128121f7
xenogearsX <xenogears@hotmail.fr>
2021-06-27 20:22:33 +0200

    create header component with logo and header components + css

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Burger from './Burger'
+import Logo from './Logo'
+
+import './Header.css'
+
+const Header = () => {
+  const [clicked, setCliked] = useState(false)
+
+  const handleClick = () => setCliked(!clicked)
+
+  return (
+    <header id='header'>
+      <Logo />
+      <Burger clicked={clicked} handleClick={handleClick} />
+    </header>
+  )
+}
+
+export default Header
