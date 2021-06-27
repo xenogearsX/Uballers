c1c095369d7b2d9c1554cc318566987226034d63
xenogearsX <xenogears@hotmail.fr>
2021-06-27 20:25:10 +0200

    create grounds component to display playgrounds and integrate select for filter + css

---
+++ src/components/Grounds.js
@@ -0,0 +1,27 @@
+import { useContext, useState } from 'react'
+import { Link } from 'react-router-dom'
+import UserContext from '../context/UserContext'
+
+import './Grounds.css'
+import Select from './Select'
+
+const Grounds = () => {
+  const groundsData = useContext(UserContext)[0]
+  const [filter, setFilter] = useState('')
+
+  return (
+    <div className='grounds'>
+      <h1>Liste des terrains</h1>
+      <Select filter={setFilter} />
+      {groundsData.allGrounds
+        .filter(ground => ground.limit.includes(filter))
+        .map(ground => (
+          <div className='groundLink' key={ground.groundId}>
+            <Link to={`/ground/${ground.groundId}`}>{ground.groundName}</Link>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export default Grounds
